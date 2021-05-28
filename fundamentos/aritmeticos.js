ae5688205a9a4fc22be3fc1a74338d8d711e1e1d
Renan-lang <renan.santosoliveira0@Outlook.com>
2021-05-28 13:30:48 -0300

    Operadores

---
+++ fundamentos/aritmeticos.js
@@ -0,0 +1,9 @@
+const [a, e, i, o, u] = [15, 4, 8, 1, 5]
+
+const soma  = a + e + i + o+ u
+const subtracao = a - i
+const multi = e * o
+const divisao = i / u
+const modulo = a % 2
+
+console.log(soma, subtracao, multi, divisao, modulo)
---
+++ fundamentos/ternario.js
@@ -0,0 +1,5 @@
+const acesso = idade => idade >= 15 ? 'Aprovado' : 'Recusado'
+
+console.log(acesso(16))
+console.log(acesso(13))
+
