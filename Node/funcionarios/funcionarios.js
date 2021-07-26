dd8706d2d40a6f90baaf648cc83271259685ead1
Renan-lang <renan.santosoliveira0@Outlook.com>
2021-07-26 15:56:50 -0300

    package.Json - Func

---
+++ Node/funcionarios/funcionarios.js
@@ -0,0 +1,23 @@
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const axios = require('axios')
+
+const china = f => f.pais === 'China'
+const mulher = f => f.genero === 'F'
+const menorSalario = (func, funcAtual) => {
+    return func.salario < funcAtual.salario ? func: funcAtual
+}
+
+axios.get(url).then(response =>{
+    const funcionarios = response.data
+    console.log(funcionarios)
+
+    //Mulher Chinesa com menor Salario
+
+    const func = funcionarios
+    .filter(china)
+    .filter(mulher)
+    .reduce(menorSalario)
+
+    console.log(func)
+})
+
