60b2ed59e61f8229b68cba8ab1936b4c1eabe30b
Renan-lang <renan.santosoliveira0@Outlook.com>
2021-07-25 21:45:55 -0300

    Modulos pastaA-B-C

---
+++ Node/pastaA/pastaB/acessarModulos.js
@@ -0,0 +1,10 @@
+const moduloA = require('../../ModuloA')
+console.log(moduloA.ola)
+
+console.log(require('./pastaC/index'))
+
+/*const http = require('http')
+http.createServer((req, res) => {
+    res.write('Bom dia')
+    res.end('Até logo')
+}).listen(8080) */
