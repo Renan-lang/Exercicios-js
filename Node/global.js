78c508a5af1c5f82db9edda4d7f436e8f3390999
Renan-lang <renan.santosoliveira0@Outlook.com>
2021-07-27 17:44:08 -0300

    global

---
+++ Node/global.js
@@ -0,0 +1,8 @@
+//console.log(global)
+
+globalThis.Minhaapp = {
+    saudacao(){
+        return 'Eu sou onipresente!'
+    },
+    nome: 'Ainda estou aqui!!!'
+}
---
+++ Node/globalClientes.js
@@ -0,0 +1,5 @@
+require('./global') 
+ console.log(Minhaapp.saudacao())
+ console.log(Minhaapp.nome)
+
+ 
